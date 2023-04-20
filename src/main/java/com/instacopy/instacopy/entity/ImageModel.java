7384147de57ac14e6d4fc1c2cb289b68fd3085e5
Kawaiiskii <antonzhadnov@gmail.com>
2023-04-20 18:07:47 +0300

    add jpa and lombook annotations

--- src/main/java/com/instacopy/instacopy/entity/ImageModel.java
+++ src/main/java/com/instacopy/instacopy/entity/ImageModel.java
@@ -1,9 +1,25 @@
 package com.instacopy.instacopy.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import net.minidev.json.annotate.JsonIgnore;
+
+@Data
+@Entity
 public class ImageModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Lob
     private byte[] imageBytes;
+
+    @JsonIgnore
     private Long userId;
+
+    @JsonIgnore
     private Long postId;
 }
