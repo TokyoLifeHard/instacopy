4cac1e4480e22706a723d050efbc6548ee11dd63
Kawaiiskii <antonzhadnov@gmail.com>
2023-04-20 17:52:39 +0300

    add jpa and lombook annotations

--- src/main/java/com/instacopy/instacopy/entity/Comment.java
+++ src/main/java/com/instacopy/instacopy/entity/Comment.java
@@ -1,16 +1,30 @@
 package com.instacopy.instacopy.entity;
 
-import jakarta.persistence.PrePersist;
+import jakarta.persistence.*;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
+@Data
+@Entity
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
     private Post post;
+
+    @Column(nullable = false)
     private String username;
+
+    @Column(nullable = false)
     private Long UserId;
+
+    @Column(columnDefinition = "text",nullable = false)
     private String mesasge;
 
+    @Column(updatable = false)
     private LocalDateTime createDate;
 
     @PrePersist
