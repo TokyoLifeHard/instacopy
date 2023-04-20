1eba19437a6c3172b0b6ed5b420b165627948059
Kawaiiskii <antonzhadnov@gmail.com>
2023-04-20 14:02:48 +0300

    Initial commit

---
+++ src/main/java/com/instacopy/instacopy/InstacopyApplication.java
@@ -0,0 +1,13 @@
+package com.instacopy.instacopy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstacopyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstacopyApplication.class, args);
+	}
+
+}
