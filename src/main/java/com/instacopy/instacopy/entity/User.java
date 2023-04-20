29fd620afa3891c64cdcb42009d3ed0ab83e71c8
Kawaiiskii <antonzhadnov@gmail.com>
2023-04-20 14:29:14 +0300

    implement User

--- src/main/java/com/instacopy/instacopy/entity/User.java
+++ src/main/java/com/instacopy/instacopy/entity/User.java
@@ -1,4 +1,29 @@
 package com.instacopy.instacopy.entity;
 
+import jakarta.persistence.PrePersist;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class User {
+
+    private Long id;
+    private String name;
+    private String lastName;
+    private String userName;
+    private String email;
+    private String biography;
+    private String password;
+
+    private Set<Role> roles = new HashSet<>();
+    private List<Post> posts = new ArrayList<>();
+    private LocalDateTime createDate;
+
+    @PrePersist
+    protected void onCreate(){
+        this.createDate = LocalDateTime.now();
+    }
 }
