8c55431731ce915ecd78c13a103f272b85fe9a66
Kawaiiskii <antonzhadnov@gmail.com>
2023-04-20 16:26:44 +0300

    implement Comment

--- src/main/java/com/instacopy/instacopy/entity/Comment.java
+++ src/main/java/com/instacopy/instacopy/entity/Comment.java
@@ -1,4 +1,20 @@
 package com.instacopy.instacopy.entity;
 
+import jakarta.persistence.PrePersist;
+
+import java.time.LocalDateTime;
+
 public class Comment {
+    private Long id;
+    private Post post;
+    private String username;
+    private Long UserId;
+    private String mesasge;
+
+    private LocalDateTime createDate;
+
+    @PrePersist
+    protected void onCreate(){
+        this.createDate = LocalDateTime.now();
+    }
 }
