c3bd6fa513fcb91d0108dad581233256d0f65d3b
Kawaiiskii <antonzhadnov@gmail.com>
2023-04-20 17:08:10 +0300

    add jpa and lombook annotations

--- src/main/java/com/instacopy/instacopy/entity/Post.java
+++ src/main/java/com/instacopy/instacopy/entity/Post.java
@@ -1,23 +1,35 @@
 package com.instacopy.instacopy.entity;
 
-import jakarta.persistence.PrePersist;
+import jakarta.persistence.*;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
+@Data
+@Entity
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String title;
     private String caption;
     private String location;
     private Integer likes;
 
+    @Column
+    @ElementCollection(targetClass = String.class)
     private Set<String> likedUsers = new HashSet<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
     private User user;
+    @OneToMany(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER, mappedBy = "post", orphanRemoval = true)
     private List<Comment> commets = new ArrayList<>();
+    @Column(updatable = false)
     protected LocalDateTime createDate;
 
     @PrePersist
