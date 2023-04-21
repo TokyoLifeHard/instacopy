0d4b2f2ff5cddf617cef6a18e8d279c7f96943a4
Kawaiiskii <antonzhadnov@gmail.com>
2023-04-21 16:11:34 +0300

    add PostRepository

---
+++ src/main/java/com/instacopy/instacopy/repository/PostRepository.java
@@ -0,0 +1,16 @@
+package com.instacopy.instacopy.repository;
+
+import com.instacopy.instacopy.entity.Post;
+import com.instacopy.instacopy.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post,Long> {
+    List<Post> findAllByUserOrderByCreatedDateDesc(User user);
+    List<Post> findAllByOrderByCreatedDateDesc();
+    Optional<Post> findPostByIdAndUser(Long id, User user);
+}
