3626b4d59306b507e9ff472b0c02443b7aa5c7d3
Kawaiiskii <antonzhadnov@gmail.com>
2023-04-23 17:30:29 +0300

    implement JWTTokenProvider

--- src/main/java/com/instacopy/instacopy/security/JWTTokenProvider.java
+++ src/main/java/com/instacopy/instacopy/security/JWTTokenProvider.java
@@ -1,4 +1,66 @@
 package com.instacopy.instacopy.security;
 
+import com.instacopy.instacopy.entity.User;
+import io.jsonwebtoken.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Component
 public class JWTTokenProvider {
+
+    public static final Logger LOG = LoggerFactory.getLogger(JWTTokenProvider.class);
+
+    public String generateToken(Authentication authentication){
+        User user = (User) authentication.getPrincipal();
+        Date now = new Date(System.currentTimeMillis());
+        Date expiryDate = new Date(now.getTime()+SecurityConstans.EXPIRATION_TIME);
+
+        String userId = Long.toString(user.getId());
+
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id",userId);
+        claims.put("username",user.getUsername());
+        claims.put("firstname",user.getName());
+        claims.put("lastname",user.getLastname());
+
+        return Jwts.builder()
+                .setSubject(userId)
+                .addClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.ES512,SecurityConstans.SECRET)
+                .compact();
+    }
+
+    public boolean validateToken(String token){
+        try {
+            Jwts.parser()
+                    .setSigningKey(SecurityConstans.SECRET)
+                    .parseClaimsJws(token);
+            return true;
+        }catch (SignatureException |
+                MalformedJwtException |
+                ExpiredJwtException |
+                UnsupportedJwtException|
+                IllegalArgumentException exception){
+            LOG.error(exception.getMessage());
+            return false;
+        }
+    }
+
+    public Long getUserIdFromToken(String token){
+        Claims claims = Jwts.parser()
+                .setSigningKey(SecurityConstans.SECRET)
+                .parseClaimsJws(token)
+                .getBody();
+        String id = claims.get("id").toString();
+        return Long.parseLong(id);
+    }
 }
