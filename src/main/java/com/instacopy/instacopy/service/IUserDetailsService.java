78b52bf905d365a007e743e7ca89a357123c406c
Kawaiiskii <antonzhadnov@gmail.com>
2023-04-23 14:53:22 +0300

    implement IUserDetailsService

--- src/main/java/com/instacopy/instacopy/service/IUserDetailsService.java
+++ src/main/java/com/instacopy/instacopy/service/IUserDetailsService.java
@@ -1,4 +1,43 @@
 package com.instacopy.instacopy.service;
 
-public class IUserDetailsService {
+
+import com.instacopy.instacopy.entity.User;
+import com.instacopy.instacopy.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class IUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    @Autowired
+    public IUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        User user = userRepository.findUserByUsername(username)
+                .orElseThrow(()->new UsernameNotFoundException("User with username "+username+" not found"));
+
+        return build(user);
+    }
+
+    public static User build(User user){
+        List<GrantedAuthority> authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.name()))
+                .collect(Collectors.toList());
+
+        return new User(user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getPassword(),
+                authorities);
+    }
 }
