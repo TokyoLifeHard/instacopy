4dba0228a71eac2a9f6ed60d19b90ec72c8d277a
Kawaiiskii <antonzhadnov@gmail.com>
2023-04-24 14:48:21 +0300

    implement InvalidLoginResponse

--- src/main/java/com/instacopy/instacopy/payload/response/InvalidLoginResponse.java
+++ src/main/java/com/instacopy/instacopy/payload/response/InvalidLoginResponse.java
@@ -1,4 +1,15 @@
 package com.instacopy.instacopy.payload.response;
 
+import lombok.Getter;
+
+@Getter
 public class InvalidLoginResponse {
+
+    private String username;
+    private String password;
+
+    public InvalidLoginResponse() {
+        this.username = "Invalid login";
+        this.password = "Invalid password";
+    }
 }
