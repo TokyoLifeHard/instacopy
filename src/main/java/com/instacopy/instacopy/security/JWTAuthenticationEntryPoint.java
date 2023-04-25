d392ea60e30d45f7e4f7221feac82f7b2b4f70ff
Kawaiiskii <antonzhadnov@gmail.com>
2023-04-25 13:42:32 +0300

    implement JWTAuthenticationEntryPoint

--- src/main/java/com/instacopy/instacopy/security/JWTAuthenticationEntryPoint.java
+++ src/main/java/com/instacopy/instacopy/security/JWTAuthenticationEntryPoint.java
@@ -1,4 +1,25 @@
 package com.instacopy.instacopy.security;
 
-public class JWTAuthenticationEntryPoint {
+import com.google.gson.Gson;
+import com.instacopy.instacopy.payload.response.InvalidLoginResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class JWTAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        InvalidLoginResponse invalidLoginResponse = new InvalidLoginResponse();
+        String jsonLoginResponse = new Gson().toJson(invalidLoginResponse);
+        response.setContentType(SecurityConstans.CONTENT_TYPE);
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.getWriter().println(jsonLoginResponse);
+    }
 }
