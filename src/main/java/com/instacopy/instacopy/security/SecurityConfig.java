9aa10451476f4b2a6e2fe4f4da352c588d54965a
Kawaiiskii <antonzhadnov@gmail.com>
2023-04-25 14:49:57 +0300

    implement SecurityConfig

--- src/main/java/com/instacopy/instacopy/security/SecurityConfig.java
+++ src/main/java/com/instacopy/instacopy/security/SecurityConfig.java
@@ -1,4 +1,61 @@
 package com.instacopy.instacopy.security;
 
-public class SecurityConfig {
+import com.instacopy.instacopy.service.IUserDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Configuration
+public class SecurityConfig{
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return new IUserDetailsService();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationEntryPoint authenticationEntryPoint(){
+        return new JWTAuthenticationEntryPoint();
+    }
+
+    @Bean
+    public OncePerRequestFilter jwtAuthenticationFilter(){
+        return new JWTAuthenticationFilter();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        return authenticationManagerBuilder.userDetailsService(userDetailsService())
+                .passwordEncoder(bCryptPasswordEncoder())
+                .and()
+                .build();
+    }
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.cors().and().csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint())
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers(SecurityConstans.SING_UP_URLS).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
 }
