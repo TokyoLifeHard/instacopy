4c35552ae473ea6b443c97365df7e25c12c01dee
Kawaiiskii <antonzhadnov@gmail.com>
2023-05-03 23:57:05 +0300

    implement PostDTO

--- src/main/java/com/instacopy/instacopy/dto/PostDTO.java
+++ src/main/java/com/instacopy/instacopy/dto/PostDTO.java
@@ -1,4 +1,18 @@
 package com.instacopy.instacopy.dto;
 
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
 public class PostDTO {
+
+    private Long id;
+    private String title;
+    private String caption;
+    private String location;
+    private String username;
+    private Integer likes;
+    private Set<String> userLiked;
+
 }
