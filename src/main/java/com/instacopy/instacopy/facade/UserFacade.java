07018acafb273073dcb39659752b6f527885ab3d
Kawaiiskii <antonzhadnov@gmail.com>
2023-05-05 23:48:38 +0300

    implement UserFacade

--- src/main/java/com/instacopy/instacopy/facade/UserFacade.java
+++ src/main/java/com/instacopy/instacopy/facade/UserFacade.java
@@ -1,4 +1,17 @@
 package com.instacopy.instacopy.facade;
 
+import com.instacopy.instacopy.dto.UserDTO;
+import com.instacopy.instacopy.entity.User;
+
 public class UserFacade {
+
+    public UserDTO userToUserDTO(User user){
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(user.getId());
+        userDTO.setFirstname(user.getName());
+        userDTO.setLastname(user.getLastname());
+        userDTO.setUsername(user.getUsername());
+        userDTO.setBiography(user.getBiography());
+        return userDTO;
+    }
 }
