8fbac6d1233c36e6d5af581aeb96f006dd007deb
Kawaiiskii <antonzhadnov@gmail.com>
2023-05-19 19:37:25 +0300

    implement CommentFacade

--- src/main/java/com/instacopy/instacopy/facade/CommentFacade.java
+++ src/main/java/com/instacopy/instacopy/facade/CommentFacade.java
@@ -1,4 +1,18 @@
 package com.instacopy.instacopy.facade;
 
+import com.instacopy.instacopy.dto.CommentDTO;
+import com.instacopy.instacopy.entity.Comment;
+import org.springframework.stereotype.Component;
+
+@Component
 public class CommentFacade {
+
+    public CommentDTO commetTocommentDTO(Comment comment){
+        CommentDTO commentDTO = new CommentDTO();
+        commentDTO.setId(comment.getId());
+        commentDTO.setUsername(comment.getUsername());
+        commentDTO.setMessage(comment.getMesasge());
+
+        return commentDTO;
+    }
 }
