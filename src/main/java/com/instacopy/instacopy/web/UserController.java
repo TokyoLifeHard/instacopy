1dcb15daaafabca47be8b58176f1cca3e21b9803
Kawaiiskii <antonzhadnov@gmail.com>
2023-05-19 20:28:23 +0300

    implement UserController

--- src/main/java/com/instacopy/instacopy/web/UserController.java
+++ src/main/java/com/instacopy/instacopy/web/UserController.java
@@ -1,4 +1,57 @@
 package com.instacopy.instacopy.web;
 
+import com.instacopy.instacopy.dto.UserDTO;
+import com.instacopy.instacopy.entity.User;
+import com.instacopy.instacopy.facade.UserFacade;
+import com.instacopy.instacopy.service.UserService;
+import com.instacopy.instacopy.validations.ResposeErrorValidation;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("api/user")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private UserFacade userFacade;
+    @Autowired
+    private ResposeErrorValidation resposeErrorValidation;
+
+    @GetMapping("/")
+    public ResponseEntity<UserDTO> getCurrentUser(Principal principal){
+        User user = userService.getCurrentUser(principal);
+        UserDTO userDTO = userFacade.userToUserDTO(user);
+
+        return new ResponseEntity<>(userDTO, HttpStatus.OK);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDTO> getUserProfile(@PathVariable("userId") String userId){
+        User user = userService.getUserById(Long.parseLong(userId));
+        UserDTO userDTO = userFacade.userToUserDTO(user);
+
+        return new ResponseEntity<>(userDTO, HttpStatus.OK);
+    }
+    @PostMapping("/update")
+    public ResponseEntity<Object> updateUser(@Valid @RequestBody UserDTO userDTO,
+                                             BindingResult bindingResult,
+                                             Principal principal){
+
+        ResponseEntity<Object> errors = resposeErrorValidation.mapValidationService(bindingResult);
+        if(!ObjectUtils.isEmpty(errors)) return errors;
+
+        User user = userService.updateUser(userDTO,principal);
+
+        UserDTO updatedUser = userFacade.userToUserDTO(user);
+        return new ResponseEntity<>(updatedUser,HttpStatus.OK);
+    }
 }
