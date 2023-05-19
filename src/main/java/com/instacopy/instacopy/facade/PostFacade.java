25dfc64262e71246386d12b53a71626bb51f38e4
Kawaiiskii <antonzhadnov@gmail.com>
2023-05-19 13:11:10 +0300

    implement PostFacade

--- src/main/java/com/instacopy/instacopy/facade/PostFacade.java
+++ src/main/java/com/instacopy/instacopy/facade/PostFacade.java
@@ -1,4 +1,23 @@
 package com.instacopy.instacopy.facade;
 
+import com.instacopy.instacopy.dto.PostDTO;
+import com.instacopy.instacopy.entity.Post;
+import org.springframework.stereotype.Component;
+
+@Component
 public class PostFacade {
+
+    public PostDTO postToPostDTO(Post post){
+
+        PostDTO postDTO = new PostDTO();
+        postDTO.setUsername(post.getUser().getUsername());
+        postDTO.setId(post.getId());
+        postDTO.setCaption(post.getCaption());
+        postDTO.setLikes(post.getLikes());
+        postDTO.setUserLiked(post.getLikedUsers());
+        postDTO.setLocation(post.getLocation());
+        postDTO.setTitle(post.getTitle());
+
+        return postDTO;
+    }
 }
